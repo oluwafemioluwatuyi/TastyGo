947683ba5f78cc020617cf180d8e0057b8ec6c4d
Oluwafemi <oluwafemioluwatuyi8@gmail.com>
2025-07-08 15:29:17 +0100

    AuthServie...

--- TastyGo/Services/AuthService.cs
+++ TastyGo/Services/AuthService.cs
@@ -1,11 +1,30 @@
-﻿using TastyGo.DTOs;
+﻿using AutoMapper;
+using TastyGo.DTOs;
 using TastyGo.Helpers;
+using TastyGo.Interfaces.IRepositories;
 using TastyGo.Interfaces.Services;
 
 namespace TastyGo.Services
 {
     public class AuthService : IAuthService
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<AuthService> _logger;
+        public AuthService(IUserRepository userRepository, IMapper mapper, IConfiguration configuration, ILogger<AuthService> logger)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _configuration = configuration;
+            _logger = logger;
+
+        }
+
+        public Task<ServiceResponse<object>> RegisterAsync(RegisterRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
         public Task<bool> EmailExistsAsync(string email)
         {
             throw new NotImplementedException();
@@ -21,10 +40,7 @@ namespace TastyGo.Services
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<object>> RegisterAsync(RegisterRequestDto request)
-        {
-            throw new NotImplementedException();
-        }
+
 
         public Task SendEmailVerificationAsync(string email)
         {
