378aa554ad26cef9b75c01fe393732dae5189b26
Oluwafemi <oluwafemioluwatuyi8@gmail.com>
2025-07-15 15:33:14 +0100

    added mapping file

---
+++ TastyGo/Helpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using TastyGo.DTOs;
+using TastyGo.Interfaces;
+using TastyGo.Models;
+
+namespace TastyGo.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<RegisterRequestDto, User>();
+
+        }
+    }
+}
